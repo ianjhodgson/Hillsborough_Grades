11884795fde767e76a5f561dad24485546a9a262
Ian Hodgson <46796537+ianjhodgson@users.noreply.github.com>
2024-03-12 10:35:12 -0400

    spatial analysis census child povertry rates

--- scripts/census_school_mapping.R
+++ scripts/census_school_mapping.R
@@ -3,7 +3,8 @@ rm(list = ls())
 library(sf)
 library(tidyverse)
 
-elem_zones <- st_read("data/shape files/Elementary")
+elem_zones <- st_read("data/shape files/Elementary") %>% 
+  mutate(district_area = st_area(.))
 school_grades <- read_csv('data/schoolgrades23_linked.csv') %>% 
   filter(charter_school == "NO", 
          alternative_ese_center_school == "N",
@@ -20,7 +21,50 @@ poverty_u18 <- poverty %>%
          npov_6_11_error = B17020012..Error,
          npov_12_17 = B17020013, 
          npov_12_17_error = B17020013..Error,
-         geometry)
+         geometry) %>% 
+  mutate(tract_area = st_area(.))
 
 ## join census and school area data
-joined <- st_covers(elem_zones, poverty_u18)
+intersect_pct <- st_intersection(elem_zones, 
+                                 poverty_u18 %>% 
+                                   filter(str_detect(name, "Census")))
+
+intersect_pct_2 <- intersect_pct %>% 
+  mutate(intersect_area = st_area(.)) %>% 
+  select(EID, name, intersect_area, district_area, tract_area) %>% 
+  st_drop_geometry() %>% 
+  mutate(coverage_share = intersect_area/tract_area) %>% 
+  group_by(name) %>% 
+  mutate(max_coverage = max(coverage_share, na.rm = T)) %>% 
+  select(EID, name, coverage_share)
+
+joined <- st_join(elem_zones, poverty_u18) %>% 
+  left_join(intersect_pct_2) %>% 
+  filter(name %>% str_detect("Census")) %>% 
+  mutate(pov_6_17 = (pov_6_11 + pov_12_17)*coverage_share, 
+         pov_6_17_error_sq = (pov_6_11_error^2 + pov_12_17_error^2)*coverage_share,
+         npov_6_17 = (npov_6_11 + npov_12_17)*coverage_share, 
+         npov_6_17_error_sq = (npov_6_11_error^2 + npov_12_17_error^2)*coverage_share) %>% 
+  st_drop_geometry() %>% 
+  group_by(EID, EID_NAME) %>% 
+  summarise(pov_6_17 = sum(pov_6_17, na.rm = T),
+            pov_6_17_error_sq = sum(pov_6_17_error_sq, na.rm = T),
+            npov_6_17 = sum(npov_6_17, na.rm = T),
+            npov_6_17_error_sq = sum(npov_6_17_error_sq, na.rm = T)) %>% 
+  mutate(pov_6_17_error = pov_6_17_error_sq^.5,
+         npov_6_17_error = npov_6_17_error_sq^.5,
+         pov_share = pov_6_17/(pov_6_17 + npov_6_17))
+
+grades <- school_grades %>% left_join(joined %>% select(school_number = EID, pov_share))
+
+grades %>% filter(type == "Elementary" | 
+                    type == "Combination", 
+                  !is.na(pov_share)) %>% 
+  select(school_name, informational_baseline_grade_2023, pov_share) %>% 
+  group_by(informational_baseline_grade_2023) %>% 
+  summarise(min_pov = min(pov_share),
+            mean_pov = mean(pov_share), 
+            max_pov = max(pov_share))
+
+grades %>% filter(informational_baseline_grade_2023 %in% c("C", "D"), !is.na(pov_share)) %>% 
+  select(school_name, grade =informational_baseline_grade_2023, pov_share) %>% View()
