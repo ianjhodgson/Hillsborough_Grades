8345061d1e3c886aaf14b930ec8212e6639bdb0f
Ian Hodgson <46796537+ianjhodgson@users.noreply.github.com>
2024-03-15 15:48:56 -0400

    Child poverty analysis

--- scripts/census_school_mapping.R
+++ scripts/census_school_mapping.R
@@ -2,8 +2,11 @@ rm(list = ls())
 
 library(sf)
 library(tidyverse)
+sf_use_s2(F)
 
 elem_zones <- st_read("data/shape files/Elementary") %>% 
+  mutate(district_area = st_area(.))
+middle_zones <- st_read("data/shape files/Middle") %>% 
   mutate(district_area = st_area(.))
 school_grades <- read_csv('data/schoolgrades23_linked.csv') %>% 
   filter(charter_school == "NO", 
@@ -23,6 +26,8 @@ poverty_u18 <- poverty %>%
          npov_12_17_error = B17020013..Error,
          geometry) %>% 
   mutate(tract_area = st_area(.))
+
+## ELEMENTARY ----
 
 ## join census and school area data
 intersect_pct <- st_intersection(elem_zones, 
@@ -66,5 +71,46 @@ grades %>% filter(type == "Elementary" |
             mean_pov = mean(pov_share), 
             max_pov = max(pov_share))
 
-grades %>% filter(informational_baseline_grade_2023 %in% c("C", "D"), !is.na(pov_share)) %>% 
-  select(school_name, grade =informational_baseline_grade_2023, pov_share) %>% View()
+grades %>% filter(informational_baseline_grade_2023 %in% c("F", "D"), !is.na(pov_share)) %>% 
+  select(school_name, grade =informational_baseline_grade_2023, pov_share) %>% write_csv("data/child_poverty_rates.csv")
+
+### Middle ---- 
+intersect_pct_mid <- st_intersection(middle_zones, 
+                                 poverty_u18 %>% 
+                                   filter(str_detect(name, "Census")))
+
+intersect_pct_2_mid <- intersect_pct_mid %>% 
+  mutate(intersect_area = st_area(.)) %>% 
+  select(MID, name, intersect_area, district_area, tract_area) %>% 
+  st_drop_geometry() %>% 
+  mutate(coverage_share = intersect_area/tract_area) %>% 
+  group_by(name) %>% 
+  mutate(max_coverage = max(coverage_share, na.rm = T)) %>% 
+  select(MID, name, coverage_share)
+
+joined_mid <- st_join(middle_zones, poverty_u18) %>% 
+  left_join(intersect_pct_2_mid) %>% 
+  filter(name %>% str_detect("Census")) %>% 
+  mutate(pov_6_17 = (pov_6_11 + pov_12_17)*coverage_share, 
+         pov_6_17_error_sq = (pov_6_11_error^2 + pov_12_17_error^2)*coverage_share,
+         npov_6_17 = (npov_6_11 + npov_12_17)*coverage_share, 
+         npov_6_17_error_sq = (npov_6_11_error^2 + npov_12_17_error^2)*coverage_share) %>% 
+  st_drop_geometry() %>% 
+  group_by(MID, MID_NAME) %>% 
+  summarise(pov_6_17 = sum(pov_6_17, na.rm = T),
+            pov_6_17_error_sq = sum(pov_6_17_error_sq, na.rm = T),
+            npov_6_17 = sum(npov_6_17, na.rm = T),
+            npov_6_17_error_sq = sum(npov_6_17_error_sq, na.rm = T)) %>% 
+  mutate(pov_6_17_error = pov_6_17_error_sq^.5,
+         npov_6_17_error = npov_6_17_error_sq^.5,
+         pov_share = pov_6_17/(pov_6_17 + npov_6_17))
+
+grades_mid <- school_grades %>% filter(district_name == "HILLSBOROUGH", type == "Middle") %>% 
+  left_join(joined_mid %>% 
+                                            select(school_number = MID, pov_share)) %>% 
+  select(school_name, school_number, informational_baseline_grade_2023, type, pov_share)
+
+grades %>% filter(type == "Elementary", district_name == "HILLSBOROUGH") %>% 
+  select(school_name, school_number, informational_baseline_grade_2023, type, pov_share) %>%
+  bind_rows(grades_mid) %>% 
+  write_csv("data/child_poverty_rates.csv")
--- scripts/school_fights.R
+++ scripts/school_fights.R
@@ -1,8 +1,27 @@
 library(tidyverse)
+library(readxl)
+
+school_grades <- read_csv('data/schoolgrades23_linked.csv') %>% 
+  # NOTE Monroe Middle was changed late from an "I" for incomeplete to a "D" school.
+  mutate(informational_baseline_grade_2023 = ifelse(school_number == "2362", "D",
+                                                    informational_baseline_grade_2023)) %>% 
+  filter(charter_school == "NO", 
+         alternative_ese_center_school == "N")
 
 enrollment_2122 <- read_xlsx("data/FLDOE/Enrollment/2122MembBySchoolByGrade.xlsx",
                              sheet = 2,
                              skip = 2) %>% 
+  janitor::clean_names() %>% 
+  filter(district %in% c("HILLSBOROUGH",
+                         "PINELLAS", 
+                         "PASCO")) %>% 
+  mutate(sch_number = as.numeric(school_number),
+         number_of_students = as.numeric(number_of_students)) %>% 
+  select(district, sch_number, number_of_students)
+
+enrollment_2223 <-read_xlsx("data/FLDOE/Enrollment/2223MembBySchoolByGrade.xlsx",
+                            sheet = 2,
+                            skip = 2) %>% 
   janitor::clean_names() %>% 
   filter(district == "HILLSBOROUGH") %>% 
   mutate(sch_number = as.numeric(school_number),
@@ -13,7 +32,9 @@ sesir <- read_xlsx("data/FLDOE/Discipline/sesir2223a-h.xlsx",
                    sheet = 2, 
                    skip = 2) %>% 
   janitor::clean_names() %>% 
-  filter(district_name == "HILLSBOROUGH") %>% 
+  filter(district_name %in% c("HILLSBOROUGH",
+                              "PINELLAS", 
+                              "PASCO")) %>% 
   left_join(school_grades %>% 
               mutate(school_number = as.numeric(school_number)) %>%
               select(district_name, school_number,
@@ -47,18 +68,18 @@ sesir_2122 <- read_xls("data/FLDOE/Discipline/sesir2122a-h.xls",
 
 sesir_join <- sesir %>% 
   bind_rows(sesir_2122) %>% 
-  arrange(sch_number, incident_type, school_year) %>% 
+  arrange(district_name, sch_number, incident_type, school_year) %>% 
   mutate(incidents_per_100 = total_incidents/number_of_students)
 
 type_totals <- sesir_join %>% 
-  group_by(school_year, incident_type) %>% 
+  group_by(district_name, school_year, incident_type) %>% 
   summarise(total_incidents = sum(total_incidents),
             incidents_leo = sum(incidents_reported_to_law_enforcement),
             students = sum(number_of_students)) %>% 
   mutate(incidents_per_100 = total_incidents/students*1000, 
          leo_per_100 = incidents_leo/students*1000, 
          leo_rate = incidents_leo/total_incidents) %>% 
-  arrange(incident_type)
+  arrange(district_name, incident_type)
 
 type_totals %>% 
   ggplot() + 
